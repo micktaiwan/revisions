18bc471b71153f4d3abd661370339b943cded9f4
Mickael Faivre-Macon <faivrem@gmail.com>
2014-05-09 12:38:45 +0200

    Updates

--- scripts/controllers/quizz.js
+++ scripts/controllers/quizz.js
@@ -5,7 +5,8 @@ angular.module('revisionsApp')
 
     $scope.question = null;
     $scope.show_form = true;
-    var questions = null;
+    var queue = null;
+    var index = 0;
 
     function findByObjectId(list, id) {
       for (var i in list) {
@@ -17,13 +18,25 @@ angular.module('revisionsApp')
     }
 
     function getNextQuestion() {
-      $scope.question = questions[Math.floor((Math.random() * questions.length))];
+      $scope.question = queue[index];
+      index += 1;
+      if (index >= queue.length) index = 0;
     }
 
+    function shuffle(sourceArray) {
+      for (var n = 0; n < sourceArray.length - 1; n++) {
+        var k = n + Math.floor(Math.random() * (sourceArray.length - n));
+
+        var temp = sourceArray[k];
+        sourceArray[k] = sourceArray[n];
+        sourceArray[n] = temp;
+      }
+    }
 
     function getQuestions() {
       Db.getQuestions(0, function(q) {
-        questions = q;
+        queue = q;
+        shuffle(queue);
         getNextQuestion();
       });
     }
